32b62cd6ba7119d2d77e65a04352607a539de918
Moe Aydin <maydin@zipcodes-MacBook-Pro-23.local>
2020-03-29 15:30:33 -0400

    part 5 complete

--- src/main/java/io/zipcoder/tc_spring_poll_application/controllers/advice/RestExceptionHandler.java
+++ src/main/java/io/zipcoder/tc_spring_poll_application/controllers/advice/RestExceptionHandler.java
@@ -3,6 +3,8 @@ package io.zipcoder.tc_spring_poll_application.controllers.advice;
 import io.zipcoder.tc_spring_poll_application.dtos.error.ErrorDetail;
 import io.zipcoder.tc_spring_poll_application.dtos.error.ValidationError;
 import io.zipcoder.tc_spring_poll_application.exception.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -17,6 +19,9 @@ import java.util.List;
 
 @ControllerAdvice
 public class RestExceptionHandler {
+
+    @Autowired
+    private MessageSource messageSource;
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException rnfe, HttpServletRequest request){
@@ -50,7 +55,7 @@ public class RestExceptionHandler {
             }
             ValidationError validationError = new ValidationError();
             validationError.setCode(fe.getCode());
-            validationError.setMessage(fe.getDefaultMessage());
+            validationError.setMessage(messageSource.getMessage(fe,null));
             validationErrorList.add(validationError);
         }
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
